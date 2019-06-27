da1a4945e3b77ae755b26de42a6d1395b2a101b9
taiki tsunekawa <s1f101702431@iniad.org>
2019-06-28 00:36:23 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftyPyString",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SwiftyPyString",
+            targets: ["SwiftyPyString"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
