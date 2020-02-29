66cf807c94b95c14204a4ec090e1ee5a491d316d
taiki tsunekawa <s1f101702431@iniad.org>
2020-03-01 00:38:46 +0900

    Update FormatTests.swift

--- Tests/SwiftyPyStringTests/FormatTests.swift
+++ Tests/SwiftyPyStringTests/FormatTests.swift
@@ -17,6 +17,19 @@ final class FormatTests : XCTestCase {
         XCTAssertEqual(str.format(1,12), "112")
         XCTAssertEqual(str.format("12",4), "124")
     }
+    func testSimpleFormatFloat() throws {
+        let str = "{}{}"
+        XCTAssertEqual(str.format(1.0,0.1), "1.00.1")
+        XCTAssertEqual(str.format(1.01,0.1), "1.010.1")
+    }
+    func testFormat() throws {
+        let str = "@@{}--{}##"
+        XCTAssertEqual(str.format(0.001,"12"), "@@0.001--12##")
+    }
+    func testFormatEscape() throws {
+        let str = "{{}}@@{}"
+        XCTAssertEqual(str.format(1), "{}@@1")
+    }
     
     /// Returns path to the built products directory.
     var productsDirectory: URL {
