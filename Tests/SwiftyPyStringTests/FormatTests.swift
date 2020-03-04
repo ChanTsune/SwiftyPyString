c098ccb9f6ff06adb24fbe27deb116083eb94c08
taiki tsunekawa <s1f101702431@iniad.org>
2020-03-04 18:22:55 +0900

    Update FormatTests.swift

--- Tests/SwiftyPyStringTests/FormatTests.swift
+++ Tests/SwiftyPyStringTests/FormatTests.swift
@@ -29,8 +29,53 @@ final class FormatTests : XCTestCase {
     func testFormatEscape() throws {
         let str = "{{}}@@{}"
         XCTAssertEqual(str.format(1), "{}@@1")
+        XCTAssertEqual("{{escape}}".format(), "{escape}")
     }
-    
+    func testEmptyFormatSpec() throws {
+        XCTAssertEqual("{:}".format(""), "")
+    }
+    func testFormatSpecConversion() throws {
+        let str = "{!a}{!s}{!r}"
+        XCTAssertEqual(str.format("a","b","c"), "'a'b'c'")
+        XCTAssertEqual(str.format(1,2,3), "123")
+        XCTAssertEqual(str.format(1.1,2.2,3.3), "1.12.23.3")
+    }
+    func testFormatPositional() throws {
+        XCTAssertEqual("{0} # {1} # {0}".format("@","&"), "@ # & # @")
+    }
+    func testStringFormatAlign() throws {
+        XCTAssertEqual("{:5}".format("s"), "s    ")
+        XCTAssertEqual("{:<5}".format("s"), "s    ")
+        XCTAssertEqual("{:^5}".format("s"), "  s  ")
+        XCTAssertEqual("{:>5}".format("s"), "    s")
+    }
+    func testIntegerFormatAlign() throws {
+        XCTAssertEqual("{:5}".format(1), "    1")
+        XCTAssertEqual("{:<5}".format(1), "1    ")
+        XCTAssertEqual("{:^5}".format(1), "  1  ")
+        XCTAssertEqual("{:>5}".format(1), "    1")
+    }
+    func testFloatFormatAlign() throws {
+        XCTAssertEqual("{:5}".format(1.1), "  1.1")
+        XCTAssertEqual("{:<5}".format(1.1), "1.1  ")
+        XCTAssertEqual("{:^5}".format(1.1), " 1.1 ")
+        XCTAssertEqual("{:>5}".format(1.1), "  1.1")
+    }
+    func testStringFormatFill() throws {
+        XCTAssertEqual("{:0<5}".format("s"), "s0000")
+        XCTAssertEqual("{:0^5}".format("s"), "00s00")
+        XCTAssertEqual("{:0>5}".format("s"), "0000s")
+    }
+    func testIntegerFormatFill() throws {
+        XCTAssertEqual("{:a<5}".format(1), "1aaaa")
+        XCTAssertEqual("{:a^5}".format(1), "aa1aa")
+        XCTAssertEqual("{:a>5}".format(1), "aaaa1")
+    }
+    func testFloatFormatFill() throws {
+        XCTAssertEqual("{:0<5}".format(1.1), "1.1aa")
+        XCTAssertEqual("{:0^5}".format(1.1), "a1.1a")
+        XCTAssertEqual("{:0>5}".format(1.1), "aa1.1")
+    }
     /// Returns path to the built products directory.
     var productsDirectory: URL {
         #if os(macOS)
